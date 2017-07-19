1f074a1868832218b298940a36cc8bfa224ed442
NaomiMariga <Kaburamariga@gmail.com>
2017-07-19 17:01:51 +0300

    define bucketlist unit tests

---
+++ app/tests/test_bucketlist.py
@@ -0,0 +1,19 @@
+import unittest
+from  models.bucketlist import Bucketlist
+
+class bucketlistTest(unittest.TestCase):  
+
+    def setUp(self):
+        self.bucketlist = Bucketlist()
+
+    def test_create_bucketlist(self):
+        self.assertEqual(self.bucketlist.create_bucketlist({"name":"movies"}), "Buckelist created successfuly")
+    def test_view_bucketlist(self):
+        self.assertEqual(self.bucketlist.edit_bucketlist({"name":"movies"}), "updated")
+    def test_edit_bucketlist(self):
+        self.assertEqual(self.bucketlist.create_bucketlist({"name":"movies"}), "Buckelist created successfuly")
+    def test_delete_bucketlist(self):
+        self.assertEqual(self.bucketlist.edit_bucketlist({"name":"movies"}), "Bucketlist deleted")
+
+    
+    
