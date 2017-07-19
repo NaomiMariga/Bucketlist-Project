25272952ee49b1f83088f405ff0510fa0d9016a6
NaomiMariga <Kaburamariga@gmail.com>
2017-07-19 16:55:59 +0300

    define the bucketlist methods

---
+++ app/models/bucketlist.py
@@ -0,0 +1,8 @@
+class Bucketlist:
+    
+    def create_bucketlist(self, name):
+        pass
+    def edit_bucketlist(self, name):
+        pass
+    def delete_bucketlist(self,name):
+        pass
