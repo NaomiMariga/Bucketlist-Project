01adbdce7d1ecf27f70d4eeef7bd6a741ba1c4d7
NaomiMariga <Kaburamariga@gmail.com>
2017-07-19 17:05:00 +0300

    define user unit tests

--- app/tests/test_user.py
+++ app/tests/test_user.py
@@ -7,9 +7,10 @@ class UserTest(unittest.TestCase):
         self.user = User()
 
     def test_register_user(self):
-        self.assertEqual(self.user.register_user({"username":"Naomi", "password":"12345", "email":"naomi@gmail.com"}), "Account created")
+        self.assertEqual(self.user.register_user({"fname":"Naomi","oname":"mariga", "email":"naomi@gmail.com","phone":"071852","username":"Naomi", "password":"12345"}), "Account created")
     def test_login_user(self):
         self.assertEqual(self.user.login_user({"username":"Naomi", "password":"12345"}), "logged in successfully")
-
-    
+    def test_invalid_login(self):
+        self.assertEqual(self.user.login_user({"username":" ","password":" "}),"Please provide username and password ")
+  
     
