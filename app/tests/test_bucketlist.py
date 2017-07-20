17941ea05682b3db8d52a3fb466937080792ad50
NaomiMariga <Kaburamariga@gmail.com>
2017-07-20 18:21:09 +0300

    modify bucketlist unit tests

--- app/tests/test_bucketlist.py
+++ app/tests/test_bucketlist.py
@@ -5,15 +5,23 @@ class bucketlistTest(unittest.TestCase):
 
     def setUp(self):
         self.bucketlist = Bucketlist()
-
+        
     def test_create_bucketlist(self):
-        self.assertEqual(self.bucketlist.create_bucketlist({"name":"movies"}), "Buckelist created successfuly")
+        self.assertEqual(self.bucketlist.create_bucketlist({
+            "name":"movies"
+            }), "Buckelist created successfuly")
     def test_view_bucketlist(self):
-        self.assertEqual(self.bucketlist.edit_bucketlist({"name":"movies"}), "updated")
+        self.assertEqual(self.bucketlist.edit_bucketlist({
+            "name":"movies"
+            }), "updated")
     def test_edit_bucketlist(self):
-        self.assertEqual(self.bucketlist.create_bucketlist({"name":"movies"}), "Buckelist created successfuly")
+        self.assertEqual(self.bucketlist.create_bucketlist({
+            "name":"movies"
+            }), "Buckelist created successfuly")
     def test_delete_bucketlist(self):
-        self.assertEqual(self.bucketlist.edit_bucketlist({"name":"movies"}), "Bucketlist deleted")
+        self.assertEqual(self.bucketlist.edit_bucketlist({
+            "name":"movies"
+            }), "Bucketlist deleted")
 
     
     
