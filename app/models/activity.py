79e1b716c329931a899ecac37168d40fb445a0bb
NaomiMariga <Kaburamariga@gmail.com>
2017-07-20 15:05:15 +0300

    Add class activity properties

--- app/models/activity.py
+++ app/models/activity.py
@@ -1,8 +1,12 @@
 class Activity:
-    
+    def __init__(self):
+        self.input_activity = ()
+        self.input_edit = input("")
+        
+
     def Add_activity(self, name):
-        pass
+        self.input_activity = (name)
     def Edit_activity(self, name):
-        pass
+        self.input_edit = (name)
     def Delete_activity(self):
-        pass
+        del()
