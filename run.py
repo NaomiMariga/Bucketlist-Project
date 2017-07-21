62e7ccf393a5144edfd8f94e0a4af2f4cc2ad869
NaomiMariga <Kaburamariga@gmail.com>
2017-07-21 16:25:38 +0300

    remove folder app and also modify the templates and run.py files

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask('__name__')
+app = Flask(__name__)
 
 @app.route('/index/', methods=['GET'])
 def index():
@@ -10,5 +10,15 @@ def index():
 def register():
     return render_template('register.html')
 
+@app.route('/', methods=['GET'])
+def bucketlist_view():
+    return render_template('bucketlist_view.html')
+
+@app.route('/', methods=['GET'])
+def bucketlist_activities():
+    return render_template('bucketlist_activities.html')
+
+
 if __name__ == "__main__":
     app.run()
+
