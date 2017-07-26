faae7eb2d8f121088b26783ee2ced29edfc4a2c5
NaomiMariga <Kaburamariga@gmail.com>
2017-07-26 15:55:25 +0300

    edit class user

--- models/user.py
+++ models/user.py
@@ -1,31 +1,7 @@
-"""
-vyguu
-"""
 class User:
     """
-    ijiuiui
+constructs the user registration and login methods
     """
-    def __init__(self):
-        self.Registered_users = {}
-        self.login = None
-    
-    def register_user(self,first_name, other_name, email, phone, username, password):
-        self.Registered_users = {
-            'first_name': first_name,
-            'other_name': other_name,
-            'email': email,
-            'phone': phone,
-            'username': username,
-            'password': password 
-        }
+    def __init__(self, Users):
+        self.Users = {}
         
-    def login_user(self,email,password):
-       for email in self.Registered_users:
-           if email == email and password == password:
-               self.login = email
-               
-
-        
-           
-
-    
