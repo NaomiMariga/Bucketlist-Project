94053f038031ea9990f2306f779d69e07422058b
NaomiMariga <Kaburamariga@gmail.com>
2017-07-27 14:06:08 +0300

    implement oop in bucketlist

--- README.md
+++ README.md
@@ -2,9 +2,26 @@
 
 ## INTRODUCTION TO THE BUCKETLIST PROJECT
 
-### TIA BC20 week 1 project: Bucketlist 
+### Bucketlist 
 
 A bucketlist help individuals to keep track of their goals and things to be done Eg. Going to a cinema, Climbing a mountain, and etc
 The bucketlist application give individuals a flexible and user-friendly interface, where they can create accounts, create and view 
 bucketlist as well as adding, updating and removing the bucketlist. The users also get to create, view, update, and delete the various 
 items they add to their bucketlists.
+
+###Running the Application
+Install the dependancies:
+	 `` "pip install -r requirements.txt" ``
+	``   python run.py``
+	
+	Running on ``host: 127.0.0.1 port: 80``
+
+###Registration
+
+
+###login 
+
+###Dashboard
+
+###
+ 
--- run.py
+++ run.py
@@ -3,12 +3,20 @@
 from flask import Flask, render_template, request, session, make_response, url_for, redirect
 import os #for cryptographic functions
 from models.bucketlist import Bucketlist
+from models.user import User
+import uuid
+
 
 app = Flask(__name__)
 
 Users = {}
+users_bucketlist = []
 app.secret_key = os.urandom(20)
-bucketlist = {}
+bucketlists_dict= {}
+
+
+
+
 
 @app.route('/register/', methods=['GET', 'POST'])
 def register():
@@ -54,7 +62,7 @@ def index():
             valid_pass = Users[email][-1]
 
             if email == valid_email and password == valid_pass:
-                session['email'] = valid_email and valid_pass
+                session['email'] = email
                 return redirect('/create_bucket/')
         except KeyError:
             error = "Login was not successful"
@@ -70,20 +78,43 @@ def bucketcreate_view():
     if request.method == 'POST':
         bucket_name = request.form.get('name')     
         print(bucket_name, "bucketlist name test")
+        bucketlist_id = str(uuid.uuid1())
+        bucketlist_object = Bucketlist(bucketlist_id,bucket_name)
 
-        bucketlist[bucket_name] = [bucket_name]
+        bucketlists_dict[session['email']] = []
+        bucketlists_dict[session['email']].append(bucketlist_object)
+        print(bucketlists_dict[session['email']])
 
-        try:
-            name_exist = bucketlist[bucket_name]
-            name_given = request.form['name']
-            if name_given != name_exist:
-                bucketlist[bucket_name] = [bucket_name]        
-                if bucketlist[bucket_name]:
-                    return render_template('bucketcreate_view.html', buckets = bucketlist)             
-        except KeyError:
-            error = "name already exists"
-            return render_template('bucketcreate_view.html', error = error) 
+        
 
+        # name_exist = bucketlist[bucket_name]
+        # print (name_exist)
+        # if bucket_name != name_exist:
+        #     bucketlist[bucket_name] = bucket_name    
+        return render_template('bucketcreate_view.html', buckets = bucketlists_dict[session['email']])             
+        # else:
+        #     error = "name already exists"
+        #     return error
+    
+@app.route('/edit_bucket/<bucketlist_id>', methods=['POST'])
+def edit_bucket(bucketlist_id):
+    if request.method == 'POST':
+        name_given = request.form.get('Edit_name')
+        print (name_given , "This is edited bucket name")
+        for bucket in bucketlists_dict[session['email']]:
+            if bucketlist_id == bucket.bucketlist_id:
+                bucket.bucket_name = name_given
+             
+        return render_template('bucketcreate_view.html', buckets = bucketlists_dict[session['email']])
+    # try:
+                
+    #     bucketlist[bucket] = [name_given]       
+    #     return render_template('bucketcreate_view.html', buckets = bucketlist)             
+    # except KeyError:
+    #     error = "name already exists"
+    #     return render_template('bucketcreate_view.html', error = error) 
+        
+@app.rout('/delete_bucket'/)
 @app.route('/activities/', methods=['GET'])
 def bucketlist_activities():
     """  redirects to the bucketlist  activities
